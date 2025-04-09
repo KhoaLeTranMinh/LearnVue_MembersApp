7b360eda8ac8ebda7a45187fd4be49a8c9c5baec
KTheAsianimeBoi <minhkhoahc2002@gmail.com>
2025-04-10 02:30:36 +0900

    Revert "small changes"
    This reverts commit 4aa40e69583e4225e4ce2b7c4de96ebc81ce988a.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
 export default tseslint.config({
-	rules: {
-		// Note: you must disable the base rule as it can report incorrect errors
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": "error",
-	},
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
 })
-
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from 'vue-router'
 
-import MembersAdminControllerPage from "@/views/MembersAdminControllerPage.vue"
+import MembersControllerPage from '@/views/MembersAdminControllerPage.vue'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/members",
-			name: "MembersPage",
-			component: MembersAdminControllerPage,
-			props: {
-				isAdmin: false,
-			},
-		},
-		{
-			path: "/admins",
-			name: "AdminsPage",
-			component: MembersAdminControllerPage,
-			props: {
-				isAdmin: true,
-			},
-		},
-	]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/members',
+      name: 'MembersPage',
+      component: MembersControllerPage,
+      props: {
+        isAdmin: false,
+      },
+    },
+    {
+      path: '/admins',
+      name: 'AdminsPage',
+      component: MembersControllerPage,
+      props: {
+        isAdmin: true,
+      },
+    },
+  ],
 })
 
-
+export default router
