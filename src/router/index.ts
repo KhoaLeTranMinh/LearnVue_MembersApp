4aa40e69583e4225e4ce2b7c4de96ebc81ce988a
KTheAsianimeBoi <minhkhoahc2002@gmail.com>
2025-04-10 02:21:25 +0900

    small changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
-import MembersControllerPage from '@/views/MembersAdminControllerPage.vue'
+import MembersAdminControllerPage from "@/views/MembersAdminControllerPage.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/members',
-      name: 'MembersPage',
-      component: MembersControllerPage,
-      props: {
-        isAdmin: false,
-      },
-    },
-    {
-      path: '/admins',
-      name: 'AdminsPage',
-      component: MembersControllerPage,
-      props: {
-        isAdmin: true,
-      },
-    },
-  ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/members",
+			name: "MembersPage",
+			component: MembersAdminControllerPage,
+			props: {
+				isAdmin: false,
+			},
+		},
+		{
+			path: "/admins",
+			name: "AdminsPage",
+			component: MembersAdminControllerPage,
+			props: {
+				isAdmin: true,
+			},
+		},
+	],
 })
 
 export default router
+{
+}
